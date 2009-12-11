55769224a0954e54597a6dbb18f748894428d6a7
Bradley W. Settlemyer <bws@deepcopy.org>
2009-12-11 10:22:13 -0500

    Wrapper script to provide forkthread like capability with XDD.

---
+++ contrib/xdd_forkthread.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+# This script has the options to perform a fork thread style test
+# At present, it is completely hard coded.  It could be altered to
+# provide a more useful interface.  It assumes the data file exists.
+#
+# Note:  The seed argument ensures that different random offsets are
+#  chosen for each invocation.
+#
+xdd_exe=xdd
+data_file=/data/xfs/tmruwart/ft.file
+
+$xdd_exe -op read \
+     -targets 1 $data_file \
+     -targets 1 $data_file \
+     -blocksize 16384 \
+     -reqsize 1 \
+     -seek range 16384000 \
+     -seek random \
+     -seek seed `date +%s` \
+     -verbose \
+     -heartbeat 1 \
+     -numreqs 8192 \
+     -verify contents \
+     -qd 4 \
+     -ts detailed \
+     -dio \
+     -targetoffset 317 \
+     -qthreadinfo \
+     -ts output xdd.ft.dio \
+     -csvout xdd.ft.dio.csv
