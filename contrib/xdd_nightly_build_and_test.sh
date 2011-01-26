101af23a437e4d0a1c3cfa66d5f85669886ba904
Bradley W. Settlemyer <settlemyerbw@ornl.gov>
2011-01-26 16:32:20 -0500

    Some minor updates to get nightly builds closer.

--- contrib/xdd_nightly_build_and_test.sh
+++ contrib/xdd_nightly_build_and_test.sh
@@ -7,8 +7,10 @@
 # Status flags
 #
 BUILD_RC=1
+CONFIG_RC=1
 INSTALL_RC=1
 TEST_RC=1
+CRAY_RC=1
 
 #
 # System locations
@@ -20,6 +22,12 @@ datestamp=$(date +%s)
 build_dir=$nightly_base_path/${USER}/$datestamp/build
 install_dir=$nightly_base_path/${USER}/$datestamp/install
 test_dir=$nightly_base_path/${USER}/$datestamp/test
+output_dir=$nightly_base_path/${USER}/$datestamp/logs
+build_log=$output_dir/build.log
+config_log=$output_dir/config.log
+install_log=$output_dir/install.log
+test_log=$output_dir/test.log
+cray_log=$output_dir/cray.log
 
 
 #
@@ -28,38 +36,52 @@ test_dir=$nightly_base_path/${USER}/$datestamp/test
 mkdir -p $build_dir
 mkdir -p $install_dir
 mkdir -p $test_dir
+mkdir -p $output_dir
 
 #
-# Build and install XDD
+# Move the results from the cray over
 #
-cd $build_dir
-git clone $xdd_repo_path
-cd xdd
-./configure --prefix=$install_dir
-make
+CRAY_RC=0
+
+#
+# Retrieve and configure XDD
+#
+cd $build_dir >>$config_log
+git clone $xdd_repo_path >>$config_log
+cd xdd >>$config_log
+./configure --prefix=$install_dir >>$config_log
+CONFIG_RC=$?
+
+#
+# Build XDD
+#
+make >>$build_log
 BUILD_RC=0
 
-make install
-INSTALL_RC=0
+#
+# Install XDD
+#
+make install >>$install_log
+INSTALL_RC=$?
 
 #
 # Run the nightly tests
 #
 export PATH=$install_dir/bin:$PATH
-if [ "$(which xdd)" = "$install_dir/bin/xdd" ]; then
+if [ "$(which xdd)" != "$install_dir/bin/xdd" ]; then
     echo "ERROR: Not using the nightly test version of XDD"
     exit 1
 fi
 
 cd $test_dir
-$build_dir/tests/run_all_tests.sh
-TEST_RC=0
+$build_dir/xdd/tests/run_all_tests.sh >>$test_log
+TEST_RC=$?
 
 #
 # Mail the results of the build and test to durmstrang-io
 #
 rcpt="durmstrang-io@email.ornl.gov"
-if [ 0 -eq $BUILD_RC -a 0 -eq $INSTALL_RC -a 0 -eq $TEST_INSTALL ]; then
+if [ 0 -eq $BUILD_RC -a 0 -eq $CONFIG_RC -a 0 -eq $INSTALL_RC -a 0 -eq $TEST_RC ]; then
     #mail -s "SUCCESS - Nightly build and test for $datestamp" "$rcpt" <<EOF
 echo <<EOF 
 Everything completed successfully as far as I can tell.
@@ -67,6 +89,7 @@ EOF
 else
     #mail -s "FAILURE - Nightly build and test for $datestamp" -a $logfile "$rcpt" <<EOF
 echo <<EOF 
+Configure return code: $CONFIG_RC
 Build return code: $BUILD_RC
 Install return code: $INSTALL_RC
 Test return code: $TEST_RC
