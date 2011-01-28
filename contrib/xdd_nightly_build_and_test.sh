86c074326f8ddebc3c847cc66501f5274c2aab8d
Bradley W. Settlemyer <settlemyerbw@ornl.gov>
2011-01-28 10:15:37 -0500

    Update nightly build script.

--- contrib/xdd_nightly_build_and_test.sh
+++ contrib/xdd_nightly_build_and_test.sh
@@ -23,11 +23,11 @@ build_dir=$nightly_base_path/${USER}/$datestamp/build
 install_dir=$nightly_base_path/${USER}/$datestamp/install
 test_dir=$nightly_base_path/${USER}/$datestamp/test
 output_dir=$nightly_base_path/${USER}/$datestamp/logs
-build_log=$output_dir/build.log
-config_log=$output_dir/config.log
-install_log=$output_dir/install.log
-test_log=$output_dir/test.log
-cray_log=$output_dir/cray.log
+build_log=$output_dir/nightly-build.log
+config_log=$output_dir/nightly-config.log
+install_log=$output_dir/nightly-install.log
+test_log=$output_dir/nightly-test.log
+cray_log=$output_dir/nightly-cray.log
 
 
 #
@@ -39,7 +39,7 @@ mkdir -p $test_dir
 mkdir -p $output_dir
 
 #
-# Move the results from the cray over
+# Check the results from the Cray 
 #
 CRAY_RC=0
 
@@ -56,7 +56,7 @@ CONFIG_RC=$?
 # Build XDD
 #
 make >>$build_log
-BUILD_RC=0
+BUILD_RC=$?
 
 #
 # Install XDD
@@ -73,8 +73,24 @@ if [ "$(which xdd)" != "$install_dir/bin/xdd" ]; then
     exit 1
 fi
 
+test_local_dir=$test_dir/local_src
+test_src_dir=$test_dir/nightly_src
+test_dest_dir=$test_dir/nightly_dest
+mkdir -p $test_src_dir
+mkdir -p $test_dest_dir
 cd $test_dir
-$build_dir/xdd/tests/run_all_tests.sh >>$test_log
+cat >nightly_test_config <<EOF
+XDDTEST_XDD_EXE=xdd.Linux
+XDDTEST_XDDCP_EXE=xddcp
+XDDTEST_XDDFT_EXE=xddft
+XDDTEST_LOCAL_MOUNT=$test_local_dir
+XDDTEST_SOURCE_MOUNT=$test_src_dir
+XDDTEST_DEST_MOUNT=$test_dest_dir
+XDDTEST_OUTPUT_DIR=$output_dir
+XDDTEST_E2E_SOURCE=localhost
+XDDTEST_E2E_DEST=localhost
+EOF
+$build_dir/xdd/tests/run_all_tests.sh nightly_test_config >>$test_log
 TEST_RC=$?
 
 #
@@ -83,16 +99,17 @@ TEST_RC=$?
 rcpt="durmstrang-io@email.ornl.gov"
 if [ 0 -eq $BUILD_RC -a 0 -eq $CONFIG_RC -a 0 -eq $INSTALL_RC -a 0 -eq $TEST_RC ]; then
     #mail -s "SUCCESS - Nightly build and test for $datestamp" "$rcpt" <<EOF
-echo <<EOF 
+echo "
 Everything completed successfully as far as I can tell.
-EOF
+"
 else
     #mail -s "FAILURE - Nightly build and test for $datestamp" -a $logfile "$rcpt" <<EOF
-echo <<EOF 
+echo "
+Cray return code: $CRAY_RC
 Configure return code: $CONFIG_RC
 Build return code: $BUILD_RC
 Install return code: $INSTALL_RC
 Test return code: $TEST_RC
-See attachment for errors.
-EOF
+Zero indicates success. See attachment for error logs.
+"
 fi
