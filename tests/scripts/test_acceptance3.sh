72296bebb88faeaa0adfa7c2fa57e8be89beb3c0
Stephen Hodson <swh13@jaguarpf-login5.ccs.ornl.gov>
2011-02-07 13:06:08 -0500

    Added tests #3 and #4 to test -timelimit and -runtime functionality. Currently these tests FAIL

---
+++ tests/scripts/test_acceptance3.sh
@@ -0,0 +1,47 @@
+#!/bin/sh 
+#
+# Acceptance test for XDD.
+#
+# Validate the funtionality of -timelimit option
+#
+#
+# Source the test configuration environment
+#
+source ./test_config
+
+# Perform pre-test 
+echo "Beginning Acceptance Test 3 . . ."
+test_dir=$XDDTEST_LOCAL_MOUNT/acceptance3
+rm -rf $test_dir
+mkdir -p $test_dir
+
+# ReqSize 4096, Bytes 1GB, Targets 1, QueueDepth 4, Passes 1
+data_file=$test_dir/test
+# write a file
+$XDDTEST_XDD_EXE -op write -reqsize 4096 -mbytes    1024 -targets 1 $data_file -qd 4                -passes 1 -datapattern random 
+# now read forever, small random I/O  with a timelimit
+$XDDTEST_XDD_EXE -op read  -reqsize    1 -mbytes   16384 -targets 1 $data_file -qd 4 -timelimit 10  -passes 1 -seek random -seek range 1024 &
+pid=$$
+echo "xdd started, pid=$pid"
+echo "sleep 30"
+sleep 30
+
+# Validate output
+test_passes=1
+pkill xdd
+if [ $? -eq 0 ]; then
+   test_passes=0
+  echo "Had to kill $pid."
+fi
+
+# Perform post-test cleanup
+rm -rf $test_dir
+
+# Output test result
+if [ "1" == "$test_passes" ]; then
+  echo "Acceptance Test 3 - -timelimit termintated XDD: PASSED."
+  exit 0
+else
+  echo "Acceptance Test 3 - -timelimit failed to terminate XDD: FAILED."
+  exit 1
+fi
