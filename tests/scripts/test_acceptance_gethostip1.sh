77eaa040d0125ceffdb699fe5c3e6bf8231c5d03
Bradley W. Settlemyer <settlemyerbw@ornl.gov>
2011-09-19 16:11:46 -0400

    Adding tests for new executables.

---
+++ tests/scripts/test_acceptance_gethostip1.sh
@@ -0,0 +1,29 @@
+#!/bin/sh 
+#
+# Acceptance test for XDD.
+#
+# Validate the gethostip program
+#
+
+#
+# Source the test configuration environment
+#
+source ./test_config
+
+#
+# Check IP of localhost
+#
+test_passes=0
+ip=$($XDDTEST_XDD_PATH/xdd-gethostip -d localhost)
+if [ "127.0.0.1" = "$ip" ]; then
+    test_passes=1
+fi
+
+# Output test result
+if [ "1" == "$test_passes" ]; then
+  echo "Acceptance Test GetHostIP 1: PASSED."
+  exit 0
+else
+  echo "Acceptance Test GetHostIP 1: FAILED."
+  exit 1
+fi
---
+++ tests/scripts/test_acceptance_truncate1.sh
@@ -0,0 +1,44 @@
+#!/bin/sh 
+#
+# Acceptance test for XDD.
+#
+# Validate the gethostip program
+#
+
+#
+# Source the test configuration environment
+#
+source ./test_config
+
+#
+# Create the file to truncate
+#
+tfile=$XDDTEST_LOCAL_MOUNT/truncfile1
+touch $tfile
+
+#
+# Truncate a file to a size
+#
+test_passes=0
+$XDDTEST_XDD_PATH/xdd-truncate -s 64 $tfile
+fsize1=$($XDDTEST_XDD_PATH/xdd-getfilesize $tfile)
+if [ 64 -eq "$fsize1" ]; then
+    test_passes=1
+fi
+
+$XDDTEST_XDD_PATH/xdd-truncate -s 4 $tfile
+fsize2=$($XDDTEST_XDD_PATH/xdd-getfilesize $tfile)
+if [ 4 -eq "$fsize2" ]; then
+    test_passes=1
+else
+    test_passes=0
+fi
+
+# Output test result
+if [ "1" == "$test_passes" ]; then
+  echo "Acceptance Test Truncate 1: PASSED."
+  exit 0
+else
+  echo "Acceptance Test Truncate 1: FAILED."
+  exit 1
+fi
