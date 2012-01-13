e3c4db82aeb9f3a3666fd2ec970a573ba1d1b168
Bradley W. Settlemyer <settlemyerbw@ornl.gov>
2012-01-13 11:06:21 -0500

    Revert tests to the last version that passed.

--- tests/scripts/test_acceptance_xddcp-w.sh
+++ tests/scripts/test_acceptance_xddcp-w.sh
@@ -2,7 +2,7 @@
 #
 # Acceptance test for XDD.
 #
-# Validate the post analysis with kernel tracing flag with xddcp for the -w flag
+# Validate the post analysis with kernel tracing flag with xddcp for the -W flag
 #
 
 #
@@ -11,15 +11,15 @@
 source ./test_config
 
 # Perform pre-test 
-echo "Beginning XDDCP Post Analysis w/o Kernel Tracing Test 1 . . ."
-test_dir=$XDDTEST_SOURCE_MOUNT/postanalysis-w
+echo "Beginning XDDCP Post Analysis w Kernel Tracing Test 1 . . ."
+test_dir=$XDDTEST_SOURCE_MOUNT/postanalysis-W
 rm -rf $test_dir
 mkdir -p $test_dir
-ssh $XDDTEST_E2E_DEST "rm -rf $XDDTEST_DEST_MOUNT/postanalysis-w"
-ssh $XDDTEST_E2E_DEST "mkdir -p $XDDTEST_DEST_MOUNT/postanalysis-w"
+ssh $XDDTEST_E2E_DEST "rm -rf $XDDTEST_DEST_MOUNT/postanalysis-W"
+ssh $XDDTEST_E2E_DEST "mkdir -p $XDDTEST_DEST_MOUNT/postanalysis-W"
 
 source_file=$test_dir/file1
-dest_file=$XDDTEST_DEST_MOUNT/postanalysis-w/file1
+dest_file=$XDDTEST_DEST_MOUNT/postanalysis-W/file1
 
 #
 # Create the source file
@@ -30,7 +30,7 @@ $XDDTEST_XDD_EXE -target $source_file -op write -reqsize 4096 -mbytes 4096 -qd 4
 # Start a long copy
 #
 export PATH=$(dirname $XDDTEST_XDD_EXE):/usr/bin:$PATH
-bash -x $XDDTEST_XDDCP_EXE -w 5 $source_file $XDDTEST_E2E_DEST:$dest_file &
+bash -x $XDDTEST_XDDCP_EXE -W 5 $source_file $XDDTEST_E2E_DEST:$dest_file &
 pid=$!
 
 wait $pid
@@ -43,17 +43,17 @@ if [ 0 -eq $rc ]; then
     # Check for existence of post analysis files with kernel tracing
     #
     test_passes=1
-    xfer_files=$(ls -1 $(hostname -s)*/xfer* | wc -l)
-    if [ "$xfer_files" != "6" ]; then
+    xfer_files=$(ls -1 ANALYSIS*/analysis.dat | wc -l)
+    if [ "$xfer_files" != "1" ]; then
 	test_passes=0
 	echo "ERROR: Failure in Post Analysis with Kernel Tracing Test 1"
-	echo "\tNumber of xfer* files is: $xfer_files, should be 6"
+        echo "\tfile analysis.dat is missing"
     fi
-    xfer_files=$(ls -1 $(hostname -s)*/xfer*eps | wc -l)
-    if [ "$xfer_files" != "5" ]; then
+    xfer_files=$(ls -1 ANALYSIS*/*eps | wc -l)
+    if [ "$xfer_files" != "18" ]; then
 	test_passes=0
 	echo "ERROR: Failure in Post Analysis with Kernel Tracing Test 1"
-	echo "\tNumber of xfer*eps files is: $xfer_files, should be 5"
+	echo "\tNumber of *eps files is: $xfer_files, should be 18"
     fi
 else
     echo "ERROR: XDDCP exited with: $rc"
@@ -62,21 +62,20 @@ fi
 # Perform post-test cleanup
 #rm -rf $test_dir
 #create directory to save all source side files
-test_dir=$XDDTEST_LOCAL_MOUNT/postanalysis-w
+test_dir=$XDDTEST_LOCAL_MOUNT/postanalysis-W
 rm   -rf           $test_dir
 mkdir -p           $test_dir
-mv $(hostname -s)* $test_dir
-mv *bin            $test_dir
-mv *csv            $test_dir
-mv *log            $test_dir
-#mv *out            $test_dir
-#mv *ascii          $test_dir
+mv xdd*            $test_dir
+mv iotrace*        $test_dir
+mv dictionary*     $test_dir
+mv ANALYSIS*       $test_dir
+mv gnuplot*        $test_dir
 
 # Output test result
 if [ "1" == "$test_passes" ]; then
-  echo "Acceptance XDDCP-w: XDDCP Post Analysis w/o Kernel Tracing - Check: PASSED."
+  echo "Acceptance XDDCP-W: XDDCP Post Analysis w Kernel Tracing - Check: PASSED."
   exit 0
 else
-  echo "Acceptance XDDCP-w: XDDCP Post Analysis w/o Kernel Tracing - Check: FAILED."
+  echo "Acceptance XDDCP-W: XDDCP Post Analysis w Kernel Tracing - Check: FAILED."
   exit 1
 fi
