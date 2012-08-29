4c0e32496dd2a7b460f39f3aa4fceb43272f15e3
Bradley W. Settlemyer <settlemyerbw@ornl.gov>
2012-08-29 13:10:42 -0400

    Fix parse spec so that it can handle multi host without errors,  and numa nodes.  Also clean up a bunch of unused variables and add a test driver for the parse function.

---
+++ tests/unit/xddcp/test_parse_endpoint_spec.bash
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+rc=0
+function assert_equals {
+    if [ "$1" != "$2" ]; then
+        echo "Assert failed: $1 != $2"
+        echo "Call stack: $FUNCNAME"
+        for ((i=0; i < ${#FUNCNAME[*]}; i=$i+1)); do
+       	    echo "INFO: Call Stack ${FUNCNAME[$i]}"
+        done
+        rc=1
+    fi
+}
+
+#
+# Source the script to get access to the functions
+#
+XDDCP_SCRIPT=$HOME/workspace/xdd/contrib/xddcp
+exit() { :; }
+source $XDDCP_SCRIPT >/dev/null
+unset -f exit
+
+
+#
+# Test variables
+#
+path=""
+rFlag=""
+user=""
+hostArray=()
+addrArray=()
+nodeArray=()
+
+#
+# Test a simple spec
+#
+spec1="user@localhost:/p1"
+parse_endpoint_spec "${spec1}" path rFlag user hostArray addrArray nodeArray
+assert_equals "/p1" $path
+assert_equals "1" $rFlag
+assert_equals "user" $user
+assert_equals "localhost" ${hostArray[*]}
+assert_equals "127.0.0.1" ${addrArray[*]}
+assert_equals "all" ${nodeArray[*]}
+
+#
+# Test a multi-host spec
+#
+spec2="user2@localhost,192.168.1.1:/p2"
+parse_endpoint_spec "${spec2}" path rFlag user hostArray addrArray nodeArray
+assert_equals "/p2" "$path"
+assert_equals "1" "$rFlag"
+assert_equals "user2" "$user"
+assert_equals "localhost 192.168.1.1" "${hostArray[*]}"
+assert_equals "127.0.0.1 192.168.1.1" "${addrArray[*]}"
+assert_equals "2" "${#hostArray[@]}"
+assert_equals "2" "${#addrArray[@]}"
+assert_equals "2" "${#nodeArray[@]}"
+
+#
+# Test a three host spec
+#
+spec3="user3@localhost,localhost,localhost:/p3"
+parse_endpoint_spec "${spec3}" path rFlag user hostArray addrArray nodeArray
+assert_equals "/p3" "$path"
+assert_equals "1" "$rFlag"
+assert_equals "user3" "$user"
+assert_equals "localhost localhost localhost" "${hostArray[*]}"
+assert_equals "127.0.0.1 127.0.0.1 127.0.0.1" "${addrArray[*]}"
+assert_equals "3" "${#hostArray[@]}"
+assert_equals "3" "${#addrArray[@]}"
+assert_equals "3" "${#nodeArray[@]}"
+
+#
+# Test a three host spec with a node
+#
+spec4="user4@localhost,localhost%1,localhost:/p4"
+parse_endpoint_spec "${spec4}" path rFlag user hostArray addrArray nodeArray
+assert_equals "/p4" "$path"
+assert_equals "1" "$rFlag"
+assert_equals "user4" "$user"
+assert_equals "localhost localhost localhost" "${hostArray[*]}"
+assert_equals "127.0.0.1 127.0.0.1 127.0.0.1" "${addrArray[*]}"
+assert_equals "all" "${nodeArray[0]}"
+assert_equals "1" "${nodeArray[1]}"
+assert_equals "all" "${nodeArray[2]}"
+assert_equals "3" "${#hostArray[@]}"
+assert_equals "3" "${#addrArray[@]}"
+assert_equals "3" "${#nodeArray[@]}"
+
+#
+# Test a four host spec with each having a node
+#
+spec5="user5@localhost%2,localhost%1,localhost%3,localhost%4:/p5"
+parse_endpoint_spec "${spec5}" path rFlag user hostArray addrArray nodeArray
+assert_equals "/p5" "$path"
+assert_equals "1" "$rFlag"
+assert_equals "user5" "$user"
+assert_equals "localhost localhost localhost localhost" "${hostArray[*]}"
+assert_equals "127.0.0.1 127.0.0.1 127.0.0.1 127.0.0.1" "${addrArray[*]}"
+assert_equals "2" "${nodeArray[0]}"
+assert_equals "1" "${nodeArray[1]}"
+assert_equals "3" "${nodeArray[2]}"
+assert_equals "4" "${nodeArray[3]}"
+assert_equals "4" "${#hostArray[@]}"
+assert_equals "4" "${#addrArray[@]}"
+assert_equals "4" "${#nodeArray[@]}"
+
+if [ 0 -eq $rc ]; then
+    echo "SUCCESS - all tests passed."
+else
+    echo "FAILURE - some tests failed."
+fi
+exit $rc
