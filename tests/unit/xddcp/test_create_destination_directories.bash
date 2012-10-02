35b5a190f0b1f6a375e2da32cb9294d60bd46190
Bradley W. Settlemyer <settlemyerbw@ornl.gov>
2012-10-02 13:25:32 -0400

    Updating to make sure that complementing the path works fairly well for recursive transfers.

--- tests/unit/xddcp/test_create_destination_directories.bash
+++ tests/unit/xddcp/test_create_destination_directories.bash
@@ -43,19 +43,61 @@ destIP=""
 destUser=""
 
 #
-# Test a file with two different entries
+# Test a file with two different relative paths
 #
-echo -e "td1/td2\ntd3/td4" > tcdd1-src
-srcFileList="tcdd1-src"
-destPath="tcdd1"
-remoteFlag=1
-destHost="localhost"
-destIP=127.0.0.1
-destUser=""
-create_destination_directories "${srcFileList}" "${destPath}" "${remoteFlag}" "${destHost}" "${destIP}" "${destUser}"
-assert_exists "tcdd1/td1" 
-assert_exists "tcdd1/td3" 
-rm -rf $srcFileList $destPath
+function test1 {
+    echo -e "td1/td2\ntd3/td4" > tcdd1-src
+    srcFileList="tcdd1-src"
+    srcPath="."
+    destPath="${PWD}/tcdd1"
+    remoteFlag=1
+    destHost="localhost"
+    destIP=127.0.0.1
+    destUser=""
+    create_destination_directories "${srcFileList}" "${srcPath}" "${destPath}" "${remoteFlag}" "${destHost}" "${destIP}" "${destUser}"
+    assert_exists "${PWD}/tcdd1/td1" 
+    assert_exists "${PWD}/tcdd1/td3" 
+    rm -rf $srcFileList $destPath
+}
+test1
+
+#
+# Test a file with two different absolute paths
+#
+function test2 {
+    echo -e "/data/xfs/td10/td20\n/data/xfs/td30/td40" > tcdd2-src
+    srcFileList="tcdd2-src"
+    srcPath="/data/xfs"
+    destPath="${PWD}/tcdd2"
+    remoteFlag=1
+    destHost="localhost"
+    destIP=127.0.0.1
+    destUser=""
+    create_destination_directories "${srcFileList}" "${srcPath}" "${destPath}" "${remoteFlag}" "${destHost}" "${destIP}" "${destUser}"
+    assert_exists "${PWD}/tcdd2/td10" 
+    assert_exists "${PWD}/tcdd2/td30" 
+    rm -rf $srcFileList $destPath
+}
+test2
+
+#
+# Test a file with two different absolute paths, but root in common
+#
+function test3 {
+    echo -e "/data/xfs/td1/td2\n/data/xfs/td3/td4" > tcdd3-src
+    srcFileList="tcdd3-src"
+    srcPath="/data"
+    destPath="${PWD}/tcdd3"
+    remoteFlag=1
+    destHost="localhost"
+    destIP=127.0.0.1
+    destUser=""
+    create_destination_directories "${srcFileList}" "${srcPath}" "${destPath}" "${remoteFlag}" "${destHost}" "${destIP}" "${destUser}"
+    assert_exists "${PWD}/tcdd3/xfs/td1" 
+    assert_exists "${PWD}/tcdd3/xfs/td3" 
+    rm -rf $srcFileList $destPath
+}
+test3
 
 #
 # Test a file with two duplicate destination paths
