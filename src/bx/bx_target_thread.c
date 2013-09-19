4e7304714112eebec260fdb7fd2a73abb55bf09b
Bradley W. Settlemyer <settlemyerbw@ornl.gov>
2013-09-19 17:01:37 -0400

    Update copyright for public release
    This updates the copyright notices in the bx module to
    protect/preserve I/O Performance and UT-Battelle source code
    copyrights.

--- src/bx/bx_target_thread.c
+++ src/bx/bx_target_thread.c
@@ -1,3 +1,15 @@
+/*
+ * XDD - a data movement and benchmarking toolkit
+ *
+ * Copyright (C) 1992-23 I/O Performance, Inc.
+ * Copyright (C) 2009-23 UT-Battelle, LLC
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License version 2, as published by the Free Software
+ * Foundation.  See file COPYING.
+ *
+ */
 /* target.c
 */
 #include <stdio.h>
--- src/bx/test_bxt.sh
+++ src/bx/test_bxt.sh
@@ -1,3 +1,15 @@
+/*
+ * XDD - a data movement and benchmarking toolkit
+ *
+ * Copyright (C) 1992-23 I/O Performance, Inc.
+ * Copyright (C) 2009-23 UT-Battelle, LLC
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License version 2, as published by the Free Software
+ * Foundation.  See file COPYING.
+ *
+ */
 # --target # <in|out|int> #WorkerThreads     <file|net|sg> <options>
 # --target # <in|out|int> #WorkerThreads     file          <filename>      <file_size>          <transfer_size>  <dio,null,sync>
 # --target # <in|out|int> #WorkerThreads     network       <client|server> <user@hostname:port> <TCP|UDP|UDT>
