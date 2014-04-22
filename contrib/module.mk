8983c9e1998c2a4849929bc8452daf6570df6ff7
Bradley W. Settlemyer <settlemyerbw@ornl.gov>
2014-04-22 15:59:31 -0400

    Modify 3rd party Python Build System
    We now include Python stuff in a slightly more sane manner.  This
    change will allow us to modify the baseline packages seamlessly,
    which in the case of paramiko at least will be necessary.

---
+++ contrib/module.mk
@@ -0,0 +1,213 @@
+#
+# This program is free software; you can redistribute it and/or modify 
+# it under the terms of the GNU General Public License as published by 
+# the Free Software Foundation; either version 2 of the License, or 
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+# for more details.
+# 
+# You should have received a copy of the GNU General Public License along 
+# with this program; if not, write to the Free Software Foundation, Inc., 
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+#
+# This file is part of XDD
+#
+# Description: XDD Contrib build rules
+#
+
+#
+# Contributed 3rd-party distributions
+#
+PARAMIKO_DIST := $(CONTRIB_DIR)/paramiko-1.13.0.tar.gz
+PYRO_DIST := $(CONTRIB_DIR)/Pyro4-4.25.tar.gz
+SERPENT_DIST := $(CONTRIB_DIR)/serpent-1.5.tar.gz
+
+#
+# Build rules for contrib
+#
+contrib: contrib/site-packages
+
+install_contrib: install_contrib_site-packages
+
+clean_contrib: clean_contrib_site-packages
+
+.PHONY: contrib install_contrib clean_contrib
+
+
+#
+# Build rules for site-packages
+#
+contrib_site-packages_pre:
+	$(MKDIR) contrib/site-packages
+
+contrib/site-packages: contrib_site-packages_pre ecdsa paramiko pycrypto pyro4 serpent
+
+install_contrib_site-packages: install_ecdsa install_paramiko install_pycrypto install_pyro4 install_serpent
+
+clean_contrib_site-packages: clean_ecdsa clean_paramiko clean_pycrypto clean_pyro4 clean_serpent
+	@$(RM) contrib/site-packages
+
+.PHONY: install_contrib_site-packages clean_contrib_site-packages contrib_site-packages_pre
+
+#
+# Build optional ecdsa
+#
+contrib/ecdsa: $(ECDSA_DIST)
+ifneq ($(ECDSA_DIST),)
+	$(MKDIR) $@
+	tar -C $@ -xvzf $< --strip-components 1
+endif
+
+contrib/site-packages/ecdsa: contrib/ecdsa
+ifneq ($(ECDSA_DIST),)
+	cd $< && $(PYTHON) setup.py build
+	cd $< && $(PYTHON) setup.py install --install-lib ../site-packages
+endif
+
+ecdsa: contrib/site-packages/ecdsa
+ifneq ($(ECDSA_DIST),)
+endif
+
+install_ecdsa: contrib/site-packages/ecdsa
+ifneq ($(ECDSA_DIST),)
+	$(CPR) $< $(INSTALL_DIR)/site-packages
+endif
+
+clean_ecdsa:
+ifneq ($(ECDSA_DIST),)
+	@echo "Cleaning the ecdsa site package"
+	@$(RM) -r contrib/site-packages/ecdsa
+	@$(RM) -r contrib/ecdsa
+	@$(RM) contrib/site-packages/ecdsa-0.11-*.egg-info
+endif
+
+.PHONY: ecdsa clean_ecdsa install_ecdsa
+
+#
+# Build paramiko
+#
+contrib/paramiko: $(PARAMIKO_DIST)
+	$(MKDIR) $@
+	tar -C $@ -xvzf $< --strip-components 1
+
+contrib/site-packages/paramiko: contrib/paramiko
+	cd $< && $(PYTHON) setup.py build
+	cd $< && $(PYTHON) setup.py install --install-lib ../site-packages --old-and-unmanageable
+
+ifneq ($(ECDSA_DIST),)
+paramiko: ecdsa 
+endif
+ifneq ($(PYCRYPTO_DIST),)
+paramiko: pycrypto
+endif
+paramiko: contrib/site-packages/paramiko
+
+install_paramiko: contrib/site-packages/paramiko
+	$(CPR) $< $(INSTALL_DIR)/site-packages
+
+clean_paramiko:
+	@echo "Cleaning the paramiko site package"
+	@$(RM) -r contrib/site-packages/paramiko
+	@$(RM) -r contrib/paramiko
+	@$(RM) -r contrib/site-packages/paramiko-1.13.0-*.egg-info
+
+.PHONY: paramiko clean_paramiko install_paramiko
+
+#
+# Build optional PyCrypto
+#
+contrib/pycrypto: $(PYCRYPTO_DIST)
+ifneq ($(PYCRYPTO_DIST),)
+	$(MKDIR) $@
+	tar -C $@ -xvzf $< --strip-components 1
+endif
+
+contrib/site-packages/Crypto: contrib/pycrypto
+ifneq ($(PYCRYPTO_DIST),)
+	cd $< && $(PYTHON) setup.py build 
+	cd $< && $(PYTHON) setup.py install --install-lib ../site-packages
+endif
+
+pycrypto: contrib/site-packages/Crypto
+ifneq ($(PYCRYPTO_DIST),)
+endif
+
+install_pycrypto: contrib/site-packages/Crypto
+ifneq ($(PYCRYPTO_DIST),)
+	$(CPR) $< $(INSTALL_DIR)/site-packages
+endif
+
+clean_pycrypto:
+ifneq ($(PYCRYPTO_DIST),)
+	@echo "Cleaning the PyCrypto site package"
+	@$(RM) -r contrib/site-packages/Crypto
+	@$(RM) -r contrib/pycrypto
+	@$(RM) contrib/site-packages/pycrypto-2.6.1-*.egg-info
+endif
+
+.PHONY: clean_pycrypto pycrypto install_pycrypto
+
+#
+# Build Pyro4
+#
+contrib/Pyro4: $(PYRO_DIST)
+	$(MKDIR) $@
+	tar -C $@ -xvzf $< --strip-components 1
+
+contrib/site-packages/Pyro4: contrib/Pyro4
+	cd $< && $(PYTHON) setup.py build
+	cd $< && $(PYTHON) setup.py install --install-lib ../site-packages --old-and-unmanageable
+
+pyro4: contrib/site-packages/serpent.py contrib/site-packages/Pyro4
+
+install_pyro4: contrib/site-packages/Pyro4
+	$(CPR) $< $(INSTALL_DIR)/site-packages
+
+clean_pyro4:
+	@echo "Cleaning the Pyro4 site package"
+	@$(RM) -r contrib/site-packages/Pyro4
+	@$(RM) -r contrib/Pyro4
+	@$(RM) -r contrib/site-packages/Pyro4-4.25-*.egg-info
+
+.PHONY: pyro4 clean_pyro4 install_pyro4
+
+#
+# Build serpent
+#
+contrib/serpent: $(SERPENT_DIST)
+	$(MKDIR) $@
+	tar -C $@ -xvzf $< --strip-components 1
+
+contrib/site-packages/serpent.py: contrib_site-packages_pre contrib/serpent
+	$(CP) contrib/serpent/serpent.py $@
+
+contrib/site-packages/serpent.pyc: contrib/site-packages/serpent.py
+	$(PYC) contrib/serpent
+	$(CP) contrib/serpent/serpent.pyc $@
+
+serpent: contrib/site-packages/serpent.py
+
+install_serpent: contrib/site-packages/serpent.pyc
+	$(CPR) $< $(INSTALL_DIR)/site-packages
+
+clean_serpent:
+	@echo "Cleaning the serpent site package"
+	@$(RM) -r contrib/site-packages/serpent.py
+	@$(RM) -r contrib/site-packages/serpent.pyc
+
+.PHONY: serpent clean_serpent install_serpent
+
+#
+#
+#
+# Build test support infrastructure
+#
+contrib/mpil: contrib/mpil.o
+	$(MPICC) $^ -o $@
+
+contrib/mpil.o: contrib/mpil.c
+	$(MPICC) -c $^ -o $@
